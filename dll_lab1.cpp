c8f8dca778e296e59a7a868269f0c824b4d5cd7f
amfexd <alexa.gerb@yandex.ru>
2023-03-21 16:17:23 +0500

    Initial state

---
+++ dll_lab1.cpp
@@ -0,0 +1,16 @@
+#pragma hdrstop
+#pragma argsused
+#include <dll_lab1.h>
+#include<string.h>
+extern "C" int _libmain(unsigned long reason)
+{
+    return 1;
+}
+extern "C" int __stdcall get_FIO(char* buf)
+{
+
+}
+extern "C" int __stdcall get_group_id()
+{
+
+}
---
+++ dll_lab1.h
@@ -0,0 +1,4 @@
+extern "C" {
+	__declspec(dllexport) int __stdcall get_fio(char*);
+	__declspec(dllexport) int __stdcall get_group_id();
+}
